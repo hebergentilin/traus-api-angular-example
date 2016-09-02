cada79ad8e710cda6eda76c6d6213a256e6b86bd
Paulo Heber <heber.gentilin@gmail.com>
2016-09-02 14:27:43 -0300

    alterado endpoint

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var host = "http://localhost:3000";
+var host = "http://traus-v2.herokuapp.com";
 // var host = "http://localhost:3000";
 
 // Declare app level module which depends on views, and components
